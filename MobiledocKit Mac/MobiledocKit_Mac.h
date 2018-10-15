46dd8e6cbcf660424202820b48a779f6d75e07ac
Samuel Goodwin <samuel@roundwallsoftware.com>
2018-10-15 18:04:11 +0200

    Mac target
    Add in a shared Mac target for people to use this. Nothing here is
    iOS-specific.

---
+++ MobiledocKit Mac/MobiledocKit_Mac.h
@@ -0,0 +1,15 @@
+////
+//  MobiledocKit
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MobiledocKit_Mac.
+FOUNDATION_EXPORT double MobiledocKit_MacVersionNumber;
+
+//! Project version string for MobiledocKit_Mac.
+FOUNDATION_EXPORT const unsigned char MobiledocKit_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobiledocKit_Mac/PublicHeader.h>
+
+
