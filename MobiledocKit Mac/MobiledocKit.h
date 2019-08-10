85706ba5a01c190b4f00f7544dd70f883a312ffe
Samuel Goodwin <samuel@roundwallsoftware.com>
2019-08-10 09:01:08 +0200

    Rename the Mac framework.
    I didn't notice this before, but it's a pain if the iOS version and Mac
    version have different names. This way when you import in both-platform
    code, it won't be so annoying.

--- MobiledocKit Mac/MobiledocKit.h
+++ MobiledocKit Mac/MobiledocKit.h
@@ -5,10 +5,10 @@
 #import <Cocoa/Cocoa.h>
 
 //! Project version number for MobiledocKit_Mac.
-FOUNDATION_EXPORT double MobiledocKit_MacVersionNumber;
+FOUNDATION_EXPORT double MobiledocKitVersionNumber;
 
 //! Project version string for MobiledocKit_Mac.
-FOUNDATION_EXPORT const unsigned char MobiledocKit_MacVersionString[];
+FOUNDATION_EXPORT const unsigned char MobiledocKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MobiledocKit_Mac/PublicHeader.h>
 
